7830e4bb6c2226e5d119549b18f0934ed93b6ead
Shardul <shardullavekar@gmail.com>
2014-07-25 15:16:47 +0530

    JSInterface added

---
+++ NativeSDK/src/main/java/com/citrus/sdk/interfaces/GetHTML.java
@@ -0,0 +1,15 @@
+package com.citrus.sdk.interfaces;
+
+import android.util.Log;
+
+/**
+ * Created by shardul on 10/7/14.
+ */
+public class GetHTML {
+
+    public void processHTML(String html) {
+
+        Log.d("HTMLPageName", html);
+    }
+
+}
